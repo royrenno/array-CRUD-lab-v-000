afb2bb067f9e87f48997e31d334c63b93ea9ac8e
roy renno <royrenno@yahoo.com>
2019-01-18 01:58:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ my_array= ["Scotch","May","Soda","Beefie"]
 end
 
 def add_element_to_end_of_array(array, element)
- add_element_to_start_of_array.push("array")
+ arry<<("array")
 end
 
 def add_element_to_start_of_array(array, element)
